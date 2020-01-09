fe2a3b1e473b3462c3907a09914c6779ca503a89
vovannmsk <vovuxa2013@yandex.ru>
2020-01-09 21:11:27 +0300

    Begin уже запускается

--- EdaShop/Startup.cs
+++ EdaShop/Startup.cs
@@ -6,29 +6,24 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+//using SportsStore.Models;
 
-namespace EdaShop
-{
-    public class Startup
-    {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
+namespace SportsStore {
+    public class Startup {
+        public Startup(IConfiguration config) => Configuration = config;
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
