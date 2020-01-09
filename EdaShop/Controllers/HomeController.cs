f6fb0fece37d05a59f992a447d6f9a92d87feb55
vovannmsk <vovuxa2013@yandex.ru>
2020-01-09 21:15:13 +0300

    2

---
+++ EdaShop/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+
+namespace EdaShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
